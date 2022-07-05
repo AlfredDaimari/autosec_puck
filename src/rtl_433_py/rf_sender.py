3bde9057380dfce723c8f7cc0241a2b583e5cd72
lakhanboy <alfreddaimari77@gmail.com>
2022-07-05 12:57:48 +0530

    structure - updated class structure
    - added new class RfMessage for rf messages
    - updated class RfSender, now used for initing rf device
    - str -> hex rep class still required

--- src/rtl_433_py/rf_sender.py
+++ src/rtl_433_py/rf_sender.py
@@ -2,37 +2,78 @@ import time, sys, rflib, struct
 from rflib import *
 from struct import *
 
+
 class RfSender:
     """
-    This class stores the message, modulation, frequency, and a few other things.
-    It also has a method to send the message.
+    This class is for initialising an RF device
     """
-    def __init__(self, message: str, modulation_type:str, baud_rate:int, packet_length:int, frequency=433920000) -> None:
-        self.message = message # The message is received as unicode characters
-        self.frequency = frequency
-        self.modulation_type = modulation_type
-        self.baud_rate = baud_rate
-        self.packet_length = packet_length
-    
-    def send_message(self):
-        d = RfCat()
-        d.setMdmModulation(MOD_2FSK)
-        d.setFreq(self.frequency)
-        d.makePktFLEN(self.packet_length) # This is the packet length.
-        d.setMdmSyncMode(0) # Disable syncword and preamble as this is not used by remote
-        d.setMdmDRate(self.baud_rate) # This sets the modulation
+
+    def __init__(self) -> None:
+        self.device = RfCat()
+
+    def send_message(self, msg: object) -> None:
+        """
+        Sends a message using the rf device \n
+        :param msg: an object of RfMessage
+        """
+        if not isinstance(msg, RfMessage):
+            raise TypeError("msg is not instance of RfMessage")
+
+        self.device.setMdmModulation(msg.modulation)
+        self.device.setFreq(self.frequency)
+        self.device.makePktFLEN(self.packet_length)
+        self.device.setMdmSyncMode(0)  # Disable syncword and preamble as this is not used by remote
+        self.device.setMdmDRate(self.baud_rate)  # This sets the modulation
         # d.setMaxPower
-        d.setModeTX() # This is the transmitter mode
+        self.device.setModeTX()  # This is the transmitter mode
 
-        KEY_DEMOD = int(self.message, 2)
-        KEY_PACK = pack(">Q", KEY_DEMOD)
-        KEY_LEN = len(KEY_PACK)
+        mod_msg = int(self.message, 2)
+        packed_msg = pack(">Q", mod_msg)
+        packed_msg_len = len(packed_msg)    # ? why is this needed
 
-        print("Sending Message...")
-        try: 
-            for i in range(10):
-                d.RFxmit(b"HELLO")
+        try:
+            self.device.RFxmit(packed_msg)
             print("Message sent!")
         except:
             print("Error in sending message!")
-        d.setModeIDLE()
+        self.device.setModeIDLE()
+
+
+class RfMessage:
+    """
+    This class is for creating a rf message to be sent using RfSender \n
+    """
+
+    def __init__(self, msg: str, mod_type: str, baud_rate: int, pk_len: int, dev:object, freq=433920000) -> None:
+        """
+        Create RfMessage \n
+        :param msg: the message to send
+        :param mod_type: the modulation type to use for the message
+        :param baud_rate: message baud_rate
+        :param pk_len: the length of the packet,
+        :param dev: the rf device (yardstick) (should be an instance of class RfSender)
+        :param freq:  the frequency in which to send the packet in
+        """
+        self.message = msg
+        self.frequency = freq
+        self.modulation_type = mod_type
+        self.baud_rate = baud_rate
+        self.packet_length = pk_len
+        self.dev = dev
+
+        self.__convert_msg_to_hex()
+
+    def __convert_msg_to_hex(self) -> None:
+        """
+        converts the message to hex
+        - Karan Handa
+        """
+        pass
+
+    def send(self) -> None:
+        """
+        sends message using the yardstick
+        """
+        self.dev.send_message(self)
+
+
