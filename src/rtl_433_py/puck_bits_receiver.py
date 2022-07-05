63f28eefa8214da3e7b4336bfc94ce0b0deed223
lakhanboy <alfreddaimari77@gmail.com>
2022-07-05 11:31:08 +0530

    feature - connected rolling_keys_fobs to ReceiveBits
    - dbus receiver function now calls rolling key fobs data structure
    - rf_cat send function still required

--- src/rtl_433_py/puck_bits_receiver.py
+++ src/rtl_433_py/puck_bits_receiver.py
@@ -15,6 +15,7 @@
 import dbus
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
+from datetime import datetime
 
 DBusGMainLoop(set_as_default=True)
 OPATH = "/org/autosec/PuckBitsReceiver"
@@ -27,11 +28,9 @@ class BitPack:
     data structure to hold a row of bits
     """
 
-    def __init__(self, bit_string: str, dist_to_prev_bitpk: int) -> None:
-        self.dist_to_prev_bitpk = dist_to_prev_bitpk
-        self.bit_pk = []
-        for bit in bit_string:
-            self.bit_pk.append(int(bit))
+    def __init__(self, bit_string: str, gap_to_prev_bitpk: int) -> None:
+        self.time_to_prev_bitpk = gap_to_prev_bitpk / 250000
+        self.bit_pk = bit_string
 
     def __len__(self) -> int:
         return len(self.bit_pk)
@@ -45,25 +44,28 @@ class KeyFobPacket:
     1 - packet fully formed, not accepting any more bits \n
     """
 
-    def __init__(self, bit_string: str, dist_to_prev_bitpk: int) -> None:
-        self.packets = [BitPack(bit_string, dist_to_prev_bitpk)]
+    def __init__(self, bit_string: str, gap_to_prev_bitpk: int) -> None:
+        self.packets = [BitPack(bit_string, gap_to_prev_bitpk)]
         self.packet_status = 0
         self.__set_packet_validity()
 
-    def __get_dist_to_prev_pkt(self) -> int:
+    def __get_dist_to_prev_pkt(self) -> float:
         """
         get key fob packet distance to previous key fob packet
         """
-        return self.packets[0].dist_to_prev_bitpk
+        return self.packets[0].time_to_prev_bitpk
 
     def __get_packet_validity(self) -> bool:
         return self.packet_status == 1
 
-    def __concatenate(self, key_fb_pkt: object) -> bool:
+    def __concatenate(self, key_fb_pkt) -> bool:
         """
         validate and concatenate to key fob packets together
         """
-        if key_fb_pkt.__get_dist_to_prev_pkt() < 120:
+        if not isinstance(key_fb_pkt, KeyFobPacket):
+            raise TypeError("key_fb_packet is not an instance of KeyFobPacket")
+
+        if key_fb_pkt.__get_dist_to_prev_pkt() < 1200:
             self.packets.append(key_fb_pkt.packets[0])
             return True
         else:
@@ -79,8 +81,12 @@ class KeyFobPacket:
         if not total_bits < 224:
             self.packet_status = 1  # packet is fully formed, should not allow appending of more bits
 
-    def concatenate(self, key_fb_pkt: object) -> bool:
-        """ concatenates two packets if the gap between them is 'very less' """
+    def concatenate(self, key_fb_pkt) -> bool:
+        """
+        concatenates two packets if the gap between them is 'very less' \n
+        :param key_fb_pkt: An instance of class KeyFobPacket
+        :return: bool : whether concatenation was successful or not
+        """
         if not self.__get_packet_validity():
             concat_ = self.__concatenate(key_fb_pkt)
             self.__set_packet_validity()
@@ -88,7 +94,10 @@ class KeyFobPacket:
         else:
             return False
 
-    def get_complete(self):
+    def get_complete(self) -> bool:
+        """
+        :return: if the key fob packet is 'valid' (has all the bit present)
+        """
         return self.__get_packet_validity()
 
 
@@ -124,13 +133,18 @@ class RollingKeyFobs:
         """
         return self.key_fobs_list.pop(0)
 
-    def push(self, bit_string: str, dist_to_prev_bitpk: int):
+    def push(self, bit_string: str, gap_to_prev_bitpk: int) -> None:
         """
-        add a new key fob to list or concatenate with previous key fob
+        add a new key fob to list or concatenate with previous key fob \n
+        :param bit_string: a string of bits
+        :param gap_to_prev_bitpk: gap to the previous received string of bits
         """
-        tmp_keyfob_pkt = KeyFobPacket(bit_string, dist_to_prev_bitpk)
-        lst_key_fob = self.key_fobs_list[-1]
-        apnd_bool = lst_key_fob.concatenate(tmp_keyfob_pkt)
+        tmp_keyfob_pkt = KeyFobPacket(bit_string, gap_to_prev_bitpk)
+        apnd_bool = False
+
+        if len(self.key_fobs_list) > 0:
+            lst_key_fob = self.key_fobs_list[-1]
+            apnd_bool = lst_key_fob.concatenate(tmp_keyfob_pkt)
 
         if not apnd_bool:
             self.key_fobs_list.append(tmp_keyfob_pkt)
@@ -149,17 +163,18 @@ class PuckBitsReceiver(dbus.service.Object):
         bus.request_name(BUS_NAME)
         bus_name = dbus.service.BusName(BUS_NAME, bus=bus)
         dbus.service.Object.__init__(self, bus_name, OPATH)
+        self.rolling_key_fobs = RollingKeyFobs()
 
     @dbus.service.method(dbus_interface=IFACE, in_signature="s", out_signature="s", sender_keyword="sender",
                          connection_keyword="conn")
-    def ReceiveBits(self, bits, sender=None, conn=None):
+    def ReceiveBits(self, bits: str, sender=None, conn=None):
         """
         receiver function for bits
         """
-        #
-        #
-        # perform bit operations
-        #
-        #
-        print(bits)
-        return bits
+        now = datetime.now()
+        dt_string = now.strftime("%H:%M:%S %d/%m/%Y")
+        print(f"received: ${bits} at ${dt_string}")
+        bit_tmp = bits.split(":")
+        bit_string = bit_tmp[0]
+        gap = int(bit_tmp[1])
+        self.rolling_key_fobs.push(bit_string, gap)
