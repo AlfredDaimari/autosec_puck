a9c1fafcaab48e477e29bf899737357a08ff927a
lakhanboy <alfreddaimari77@gmail.com>
2022-07-14 15:50:24 +0530

    feature - toyota car config [UNSTABLE]
    - checks if key fob packet is from toyota
    - sends toyota bits now to py prog

--- src/cars/toyota_innova.c
+++ src/cars/toyota_innova.c
@@ -4,17 +4,31 @@
 
 #include "decoder.h"
 #include "toyota_innova.h"
+#include "concat.h"
 
 int check_for_toyota(bitbuffer_t *bitbuffer)
 {
-        if (bitbuffer->bits_per_row[0] < 230) {
+        if (bitbuffer->bits_per_row[0] > 231) {
                 return 0;
         }
 
-        return 1;
+        return -1;
 }
 
 void create_toyota_bit_pk(bitbuffer_t *bitbuffer, char *str)
 {
+        int st = 0;
+        for (int row = 0; row < bitbuffer->num_rows; row++) {
+                concat_bits(bitbuffer->bb[row], bitbuffer->bits_per_row[row], &st, str);
+                str[st] = ':';
+                st += 1;
 
+                concat_gap(bitbuffer->gaps_per_row[row], &st, str);
+                str[st] = '-';
+                st += 1;
+
+        }
+
+        char toyota[18] = {"toyota_innova"};
+        concat_name(toyota, 14, st, str);
 }
